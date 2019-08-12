174f1c6bb26af9b963b6fc77dff33bbe15e7d853
plasmaOak <44479995+plasmaOak@users.noreply.github.com>
2019-08-11 21:22:34 -0400

    Create manageusers.php

---
+++ html/admin/manageusers.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html> 
+
+<html>
+<head>
+<title>Oak University Library</title>
+<link href="https://fonts.googleapis.com/css?family=Allura&display=swap" rel="stylesheet">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<link href="../../css/style.css" rel="stylesheet">
+<link href="../../css/responsive_solution.css" rel="stylesheet" media="screen and (max-width: 960px)"> 
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<header><h1>Oak University Library</h1></header>
+<nav>
+			<ul>
+			<li><a href="manageusers.php">Manage Users</a></li>
+			<li><a href="managebooks.php">Manage Books</a></li>
+			<li><a href="reports.php">Reports</a></li>
+			<button type="submit">Submit</button>
+			<input type="search" placeholder="Search..">
+                        <br></br>
+			</ul>
+		
+</nav>
+
+
+<nav id="login">
+	<ul>
+		<li><a href="aborrow.php">Borrow</a></li>
+		<li><a href="areturn.php">Return</a></li>
+		<li><a href="apayfees.php">Pay Fees</a></li>
+		<li><a href="acreateaccount.php">Create an Account</a></li>
+                <li><a href="aupdateaccount.php">Update an Account</a></li>
+	</ul>
+</nav>
+
+
+<footer><h3>Hours of Operation: <br></br> Monday: <br></br> Tuesday: <br></br> Wednesday
+ 				<br></br> Thursday: <br></br> Friday: <br></br> </h3>
+ 
+</footer>
+</div>
+
+</body>
+</html>
