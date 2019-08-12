5ed59b22820a96d00d0b91057fb493f0c708bf0a
plasmaOak <44479995+plasmaOak@users.noreply.github.com>
2019-08-11 21:23:11 -0400

    Create reports.php

---
+++ html/admin/reports.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html> 
+
+<html>
+<head>
+<title>Oak University Library</title>
+<link href="https://fonts.googleapis.com/css?family=Allura&display=swap" rel="stylesheet">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<link href="../../css/style.css" rel="stylesheet">
+<link href="../../css/responsive_solution.css" rel="stylesheet" media="screen and (max-width: 960px)"> 
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<header><h1>Oak University Library</h1></header>
+<nav>
+			<ul>
+			<li><a href="manageusers.php">Manage Users</a></li>
+			<li><a href="managebooks.php">Manage Books</a></li>
+			<li><a href="reports.php">Reports</a></li>
+			<button type="submit">Submit</button>
+			<input type="search" placeholder="Search..">
+                        <br></br>
+			</ul>
+		
+</nav>
+
+
+<nav id="login">
+	<ul>
+		<li><a href="latefees.php">Users with Late Fees</a></li>
+		<li><a href="currentlyborrowed.php">Books Currently Borrowed</a></li>
+		<li><a href="lrcategory.php">Lending Rates by Category</a></li>
+		<li><a href=lrauthor.php">Lending Rates by Author</a></li>
+                <li><a href="lrbook.php">Lending Rates by Book</a></li>
+                <li><a href="bbteachers.php">Books Borrowed by Teachers</a></li>
+                <li><a href="bbstudents.php">Books Borrowed by Students</a></li>
+                <li><a href="bbdepartment.php">Books Borrowed by Department</a></li>
+	</ul>
+</nav>
+
+
+<footer><h3>Hours of Operation: <br></br> Monday: <br></br> Tuesday: <br></br> Wednesday
+ 				<br></br> Thursday: <br></br> Friday: <br></br> </h3>
+ 
+</footer>
+</div>
+
+</body>
+</html>
