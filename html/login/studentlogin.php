9453a7413f7a2f0561a54a92507420a1e4faa291
plasmaOak <44479995+plasmaOak@users.noreply.github.com>
2019-11-24 17:41:07 -0500

    Update studentlogin.php

--- html/login/studentlogin.php
+++ html/login/studentlogin.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-<title>Oak University Library</title>
+<title>WonderToast</title>
 <link href="https://fonts.googleapis.com/css?family=Allura&display=swap" rel="stylesheet">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
@@ -14,22 +14,17 @@
 
 <div class="wrapper">
 
-<header><h1>Oak University Library</h1></header>
+<header><h1>WonderToast</h1></header>
 <nav>
 			<ul>
-			<li><a href="../main/login.html">Login</a></li>
-			<li><a href="../main/browse.php">Browse</a></li>
-			<li><a href="../main/about.html">About Us</a></li>
-			<button type="submit">Submit</button>
-			<input type="search" placeholder="Search..">
-                        <br></br>
+                        <br>
 			</ul>
 		
 </nav>
 
-<h2> Student Login </h2>
+<h2> User Login </h2>
 
-<p>Please fill in your credentials to login.</p>
+<p class="formp">Please fill in your credentials to login.</p>
         <form action="studentlogin.php" method="post">
             <div>
                 <label>Email</label>
@@ -58,7 +53,7 @@
     
     $query  = "SELECT Email, Password FROM User WHERE Email LIKE $email AND Password LIKE $password AND Usertype LIKE Student";
     $result = $conn->query($query);
-    header("location: ../main/browse.php");
+    header("location: ../main/home.html");
   }
   
   $result->close();
@@ -70,9 +65,7 @@
   }
 ?>
 
-
-<footer><h3>Hours of Operation: <br></br> Monday: <br></br> Tuesday: <br></br> Wednesday
- 				<br></br> Thursday: <br></br> Friday: <br></br> </h3>
+<footer><h3> © 2019 WonderToast. All rights reserved. </h3>
  
 </footer>
 </div>
