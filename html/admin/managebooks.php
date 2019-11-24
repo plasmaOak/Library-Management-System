b97c06e479685fc1725e8fbc8da6aa1694217b86
plasmaOak <44479995+plasmaOak@users.noreply.github.com>
2019-11-24 17:46:37 -0500

    Update managebooks.php

--- html/admin/managebooks.php
+++ html/admin/managebooks.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-<title>Oak University Library</title>
+<title>WonderToast</title>
 <link href="https://fonts.googleapis.com/css?family=Allura&display=swap" rel="stylesheet">
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
@@ -14,14 +14,11 @@
 
 <div class="wrapper">
 
-<header><h1>Oak University Library</h1></header>
+<header><h1>WonderToast</h1></header>
 <nav>
 			<ul>
-			<li><a href="manageusers.php">Manage Users</a></li>
+			<li><a href="manageprofile.php">Manage Profile</a></li>
 			<li><a href="managebooks.php">Manage Books</a></li>
-			<li><a href="reports.php">Reports</a></li>
-			<button type="submit">Submit</button>
-			<input type="search" placeholder="Search..">
                         <br></br>
 			</ul>
 		
@@ -37,9 +34,7 @@
 </nav>
 
 
-<footer><h3>Hours of Operation: <br></br> Monday: <br></br> Tuesday: <br></br> Wednesday
- 				<br></br> Thursday: <br></br> Friday: <br></br> </h3>
- 
+	<footer><h3> © 2019 WonderToast. All rights reserved. </h3> 
 </footer>
 </div>
 
